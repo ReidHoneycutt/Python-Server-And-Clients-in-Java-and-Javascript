d3c62d4ab35670b638f22fb43a736f7b86e578b5
ReidHoneycutt <30945205+ReidHoneycutt@users.noreply.github.com>
2024-01-18 19:59:25 -0800

    Update python_server.py

--- python_server.py
+++ python_server.py
@@ -4,10 +4,15 @@ import os
 import time
 
 async def send_random_data(websocket, path):
-    # wait for 3 seconds
-    await asyncio.sleep(3)
-    random_bytes = b'AAAA'
+    # Wait for a few seconds
+    await asyncio.sleep(3)  # Waits for 3 seconds
+
+    # Generate a random byte string of length 10
+    random_bytes = b'AAAAAA'
+
+    # Send the random byte string to the client
     await websocket.send(random_bytes)
+
 start_server = websockets.serve(send_random_data, "127.0.0.1", 8080)
 
 asyncio.get_event_loop().run_until_complete(start_server)
