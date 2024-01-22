38e327bded886114aed94c0e92fe597b0cb63cb2
ReidHoneycutt <30945205+ReidHoneycutt@users.noreply.github.com>
2024-01-22 03:07:31 -0800

    Update client.js

--- client.js
+++ client.js
@@ -1,19 +1,9 @@
 const socket = new WebSocket("ws://127.0.0.1:8080");
 
 socket.addEventListener('open', function (event) {
-    console.log("Connected");
+  console.log("connected");
 });
 
 socket.addEventListener('message', function (event) {
-    if (event.data instanceof Blob) {
-        // If the data is a Blob (binary data), read it as text
-        const reader = new FileReader();
-        reader.onload = function() {
-            console.log("Message from server: ", reader.result);
-        };
-        reader.readAsText(event.data);
-    } else {
-        // If the data is not a Blob, log it directly
-        console.log("Message from server: ", event.data);
-    }
+  console.log("Message from server: ", event.data);
 });
